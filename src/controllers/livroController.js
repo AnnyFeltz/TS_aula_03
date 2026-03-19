3430c52f9fd3489737cd4c6d2c5850a004b5dd6e
CarolFeltz <anafeltz05@gmail.com>
2026-03-19 09:26:28 -0300

    try catch pq o prof adicionou tbm

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,22 +2,29 @@ const { criarLivro } = require('../services/livroService');
 const { buscarLivroId } = require('../services/livroService');
 
 const criar = async (req, res) => {
-    const { titulo, autor} = req.body;
+    try {
+        const { titulo, autor} = req.body;
 
-    if(!titulo || !autor) return res.status(400)
-        .json({ error: 'Título e autor são obrigatórios' });
+        if(!titulo || !autor) return res.status(400).json({ error: 'Título e autor são obrigatórios' });
 
-    const livro = await criarLivro(titulo, autor);
-    return res.status(201).json(livro);
+        const livro = await criarLivro(titulo, autor);
+        return res.status(201).json(livro);
+    }catch (error) {
+        return res.status(500).json({ error: 'Ocorreu um erro ao criar o livro' });
+    }
 }
 
 const buscarId = async (req, res) => {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const livro = await buscarLivroId(id);
-    if (!livro) return res.status(404).json({ error: 'Livro não encontrado' });
+        const livro = await buscarLivroId(id);
+        if (!livro) return res.status(404).json({ error: 'Livro não encontrado' });
 
-    return res.status(200).json(livro);
+        return res.status(200).json(livro);
+    } catch (error) {
+        return res.status(500).json({ error: 'Ocorreu um erro ao buscar o livro' });
+    }
 }
 
 module.exports = { criar, buscarId };
