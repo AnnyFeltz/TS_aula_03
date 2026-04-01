02f61b7b381c11af4a4d8bbcaa0c177afc445a4c
yasmim russi mariano <yasmimrussi@gmail.com>
2026-04-01 09:26:57 -0300

    routes

---
+++ src/routes/emprestimoRoutes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const { criar, listar, atualizar, buscarId, deletar } = require('../controllers/emprestimoController');
+
+const router = Router();
+
+router.post('/criar', criar); //Anny
+//mudar para ser só disponiveis
+router.get('/', listar); //Anny
+router.get('/:id', buscarId); //Anny
+router.patch('/atualizar/:id', atualizar); //Yasmim
+router.delete('/deletar/:id', deletar); //Yasmim
+
+module.exports = router;
