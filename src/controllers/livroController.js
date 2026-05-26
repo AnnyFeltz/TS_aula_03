6e7d920557fcb733cfbccd8fe33c9071c0a393f9
CarolFeltz <anafeltz05@gmail.com>
2026-05-26 09:39:32 -0300

    agr ta funcionando o front

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -17,8 +17,6 @@ const listar = async (_, res) => {
     try {
         const livros = await listarLivros();
 
-        if (livros.length === 0) return res.status(404).json({ error: 'Nenhum livro encontrado' });
-
         return res.status(200).json(livros);
     }catch (error) {
         return res.status(500).json({ error: 'Ocorreu um erro ao listar os livros' });
